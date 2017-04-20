dd88430b66cafddf76e39784b806bc2af10c610d
paradisecr <704645806@qq.com>
2017-04-20 11:48:45 +0800

    添加：字符串替换、逆转链表、重建二叉树测试。

--- src/test/java/me/rui/sword/basic/BasicTest.java
+++ src/test/java/me/rui/sword/basic/BasicTest.java
@@ -3,11 +3,58 @@ package me.rui.sword.basic;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by cr on 2017/4/19.
  */
 public class BasicTest {
 
+
+    @Test
+    public void reConstructBinaryTreeTest() {
+        int[] pre = new int[]{1, 2, 4, 7, 3, 5, 6, 8};
+        int[] in = new int[]{4, 7, 2, 1, 5, 3, 8, 6};
+        ReConstructBinaryTree reConstructBinaryTree = new ReConstructBinaryTree();
+        ReConstructBinaryTree.TreeNode tree = reConstructBinaryTree.reConstructBinaryTree(pre, in);
+        int[] preArray = reConstructBinaryTree.preTraverse(tree);
+        int[] inArray = reConstructBinaryTree.inTraverse(tree);
+        Assert.assertArrayEquals(pre, preArray);
+        Assert.assertArrayEquals(in, inArray);
+    }
+
+    @Test
+    public void printListFromTailToHeadTest() {
+        PrintListFromTailToHead.ListNode root = null;
+        PrintListFromTailToHead.ListNode cur = null;
+        ArrayList<Integer> expectedList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            if (i == 0) {
+                root = new PrintListFromTailToHead.ListNode(5 - i);
+                cur = root;
+                expectedList.add(cur.val);
+            } else {
+                cur.next = new PrintListFromTailToHead.ListNode(5 - i);
+                cur = cur.next;
+                expectedList.add(cur.val);
+            }
+        }
+        Collections.reverse(expectedList);
+        Assert.assertEquals("Should equal.", expectedList, new PrintListFromTailToHead().printListFromTailToHead(root));
+    }
+
+    @Test
+    public void replaceSpaceTest() {
+        String srcStr = "We are happy.";
+        String expectedStr = "We%20are%20happy.";
+        ReplaceSpace replaceSpace = new ReplaceSpace();
+        Assert.assertEquals("Should equal", expectedStr, replaceSpace.replaceSpace(srcStr));
+    }
+    /**
+     * 二维数组中的查找测试
+     */
     @Test
     public void findInPartiallySortedMatrixTest() {
         int[][] array = new int[][]{
