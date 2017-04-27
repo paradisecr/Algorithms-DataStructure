f0db567deeb93475cc276cf64fbd9f6c223319c3
paradisecr <704645806@qq.com>
2017-04-27 19:18:14 +0800

    和为S的连续正数序列

--- src/main/java/me/rui/sword/comprehensive/FindContinuousSequence.java
+++ src/main/java/me/rui/sword/comprehensive/FindContinuousSequence.java
@@ -4,24 +4,37 @@ import java.util.ArrayList;
 
 /**
  * Created by cr on 2017/4/24.
+ * 和为S的连续正数序列
  */
 public class FindContinuousSequence {
     public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-        if (sum <= 2) return result;
-        int nums = 2;
-        int curNums = 2;
-        while (true) {
-            //1. 奇数
-            if (curNums % 2 == 1) {
-                // 1.若能整除，可能有可行解;
-                if (sum % curNums != 0) continue; // TODO 判断范围
-//                int left =
+        ArrayList<Integer> list;
+        if (sum <= 1) {
+            return result;
+        }
+        int bound = (sum+1)/2;
+        int left = 1;
+        int right = 2;
+        int count = 0;
+        while (right <= bound) {
+            count = 0;
+            for (int i = left; i<=right; i++) {
+                count+=i;
             }
-            //2. 偶数
-            if (curNums %2 == 0){
-
+            if (count < sum) {
+                right++;
+            } else if (count > sum){
+                left++;
+            } else {
+                list = new ArrayList<>();
+                for (int j = left; j<=right; j++) {
+                    list.add(j);
+                }
+                result.add(list);
+                right++;
             }
         }
+        return result;
     }
 }
